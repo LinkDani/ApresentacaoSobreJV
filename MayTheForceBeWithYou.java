322dd40cb27d77c74d50d1ef5723bcb1e999148a
Marmitt <lmateus.marmitt@gmail.com>
2017-05-30 21:34:33 -0300

    First Commit

---
+++ MayTheForceBeWithYou.java
@@ -0,0 +1,18 @@
+class MayTheForceBeWithYou {
+  public static void main(String[] args) {
+
+    boolean tricky = true;
+
+    if(5 < 2)
+    {
+
+    System.out.println("May The Force Be With You");
+
+    }
+    else
+
+    System.out.println("Coffee is better than tea");
+
+
+  }
+}
